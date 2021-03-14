813e7f4b1ff191d5b5e171dc5531dd93c66e5cb4
Helena <helenasleterkesto@gmail.com>
2021-03-14 14:55:35 +0100

    Js function tested with no errors

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
-const email = document.GetElementById("email");
+const emailField = document.getElementById("emailField");
 const button = document.getElementById("button");
 const response = document.getElementById("response");
 
 // Listens for submit to be clicked
 // If nonvalid, notify user
 button.addEventListener("click", function () {
-  let mail = email.value;
-  if (validateMail(mail)) {
+  let email = emailField.value;
+  if (validateEmail(email)) {
     response.innerHTML = "You have successfully submitted your request!";
   } else {
     response.innerHTML = "Oops! Please check your email";
@@ -16,7 +16,7 @@ button.addEventListener("click", function () {
 });
 
 // On submit, check for valid email
-function validateMail(mail) {
+function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(string(mail).toLowerCase());
+  return re.test(String(email).toLowerCase());
 }
