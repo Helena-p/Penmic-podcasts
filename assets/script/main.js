b9d79f43dd9815ef50a157f5a3f71b28022c627b
Helena <helenasleterkesto@gmail.com>
2021-03-16 10:16:10 +0100

    Add second id class to second form

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,6 +5,10 @@ const emailField = document.getElementById("emailField");
 const button = document.getElementById("button");
 const response = document.getElementById("response");
 
+const emailInput = document.getElementById("emailInput");
+const btn = document.getElementById("btn");
+const reply = document.getElementById("reply");
+
 // Listens for submit to be clicked
 // If nonvalid, notify user
 button.addEventListener("click", function () {
@@ -16,8 +20,22 @@ button.addEventListener("click", function () {
   }
 });
 
+btn.addEventListener("click", function () {
+  let mail = emailInput.value;
+  if (validateMail(mail)) {
+    response.innerHTML = "";
+  } else {
+    response.innerHTML = "Oops! Please check your email";
+  }
+});
+
 // On submit, check for valid email
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+function validateMail(mail) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(mail).toLowerCase());
 }
