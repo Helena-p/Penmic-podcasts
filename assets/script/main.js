62540c7779d3aed57cc99e6084552ae7cfa4e3cc
Helena <helenasleterkesto@gmail.com>
2021-03-18 12:18:58 +0100

    Deleted form for large devices, altered design on form for smaller devices to also suit bigger screens and refactor code. Change order of podcast links in larger screens

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,30 +1,9 @@
 /*jshint esversion: 6*/
 "use strict";
 
-const emailField = document.getElementById("emailField");
-const button = document.getElementById("button");
-const response = document.getElementById("response");
-
 const emailInput = document.getElementById("emailInput");
 const btn = document.getElementById("btn");
 const reply = document.getElementById("reply");
-
-// Listens for submit to be clicked
-// If nonvalid, notify user
-button.addEventListener("click", function () {
-  let email = emailField.value;
-  if (validateEmail(email)) {
-    response.innerHTML = "";
-  } else {
-    response.innerHTML = "Oops! Please check your email";
-  }
-});
-
-// On submit, check for valid email
-function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
 
 // Listens for submit to be clicked
 // If nonvalid, notify user
